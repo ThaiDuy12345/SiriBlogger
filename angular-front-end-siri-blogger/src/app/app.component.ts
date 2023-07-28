05178ce23e79d94ddbf5739112b5baf99b8d07d7
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2023-07-28 12:45:06 +0700

    testing notification in discord

--- angular-front-end-siri-blogger/src/app/app.component.ts
+++ angular-front-end-siri-blogger/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  Title = "Đây là project angular đầu tiên của tôi"
+  Title = "Đây là project angular đầu tiên của tôi, hehe"
 }
